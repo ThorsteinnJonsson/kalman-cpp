577b518a4d2fea5d14f6c9a7beb6606584fdc571
ThorsteinnJonsson <thorsteinnbaldvin@gmail.com>
2020-12-21 14:28:18 +0900

    Apply clang-format changes

--- examples/src/common/ball_simulator.h
+++ examples/src/common/ball_simulator.h
@@ -7,9 +7,11 @@
 
 class BallSim {
  public:
-  BallSim(const Eigen::Vector2f& initial_pos, const Eigen::Vector2f& initial_vel, const Eigen::Vector2f& noise);
+  BallSim(const Eigen::Vector2f& initial_pos,
+          const Eigen::Vector2f& initial_vel,
+          const Eigen::Vector2f& noise);
 
-  Eigen::Vector2f Update(float dt, float vel_wind=0.0f);
+  Eigen::Vector2f Update(float dt, float vel_wind = 0.0f);
 
   inline bool AboveGround() const { return pos_(1) > 0.0f; }
 
@@ -17,11 +19,11 @@ class BallSim {
   inline float RandFloat() { return dist_(gen_); }
 
   inline float DragForce(float velocity) {
-    const float B_m = 0.0039f + 0.0058f / (1.f + std::exp((velocity-35.f)/5.f));
+    const float B_m = 0.0039f + 0.0058f / (1.f + std::exp((velocity - 35.f) / 5.f));
     return B_m * velocity;
   }
 
- private: 
+ private:
   Eigen::Vector2f pos_;
   Eigen::Vector2f vel_;
   const Eigen::Vector2f noise_;
@@ -30,5 +32,4 @@ class BallSim {
   std::normal_distribution<float> dist_;
 };
 
-
-#endif // KALMAN_CPP_EXAMPLES_COMMON_BALL_SIMILATOR_H
+#endif  // KALMAN_CPP_EXAMPLES_COMMON_BALL_SIMILATOR_H
